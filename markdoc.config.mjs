3d22f454e971e007ea46969013314c81d54fac14
dan <dan@omux.dev>
2024-01-24 16:44:00 +0000

    add biological age blog post v1

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,29 +1,39 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config'
-import Youtube from '@components/Youtube.astro'
-import Aside from '@components/Aside.astro'
-import Gallery from '@components/blog/Gallery.astro'
+import { component, defineMarkdocConfig } from '@astrojs/markdoc/config';
+import shiki from '@astrojs/markdoc/shiki';
 
 export default defineMarkdocConfig({
+  extends: [
+    shiki({
+      theme: "dracula-soft",
+      wrap: true
+    }),
+  ],
   tags: {
     aside: {
-        render: Aside,
+        render: component('@components/Aside.astro'),
         attributes: {
             type: { type: String },
             title: { type: String },
         },
     },
     youtube: {
-      render: Youtube,
+      render: component('@components/Youtube.astro'),
       attributes: {
         url: { type: String },
         label: { type: String } 
       }
     },
     gallery: {
-      render: Gallery,
+      render: component('@components/blog/Gallery.astro'),
       attributes: {
         id: { type: String },
       }
-    }
+    },
+    katex: {
+      render: component('@components/Katex.astro'),
+      attributes: {
+        expression: { type: String },
+      }
+    },
   },
 })
